4b300e56bbe9c12c1622adf32b5e2b7c6c55090c
t-tullis <t.tullis34@gmail.com>
2019-04-05 08:51:40 -0700

    fixed auth file to require secret and changed token to require headers auth

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,8 +1,5 @@
 const jwt = require('jsonwebtoken');
-
-const jwtKey =
-  process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+const secret = require('../api/secrets.js').jwtSecret;
 
 // quickly see what this file exports
 module.exports = {
@@ -11,10 +8,10 @@ module.exports = {
 
 // implementation details
 function authenticate(req, res, next) {
-  const token = req.get('Authorization');
+  const token = req.headers.authorization;
 
   if (token) {
-    jwt.verify(token, jwtKey, (err, decoded) => {
+    jwt.verify(token, secret, (err, decoded) => {
       if (err) return res.status(401).json(err);
 
       req.decoded = decoded;
